d0e3ed6a3175923682663e3830d779f0a9b26e7d
Black <felipepessoa404@gmail.com>
2026-01-26 16:44:38 -0300

    Add end-of-file comment to ex011.js
    Added a comment '///////fim' at the end of the file to indicate the end of the script. No functional changes were made.

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -2,9 +2,10 @@ var idade = 18
 if (idade < 16) {
     console.log(`não vota`)
 
-}else if (idade < 18 || idade > 65){
+} else if (idade < 18 || idade > 65){
     console.log(`voto opcional`)
     
-}else if (idade >= 18){
+} else if (idade >= 18){
     console.log(`voto obrigatório`)
-}
+    
+}/////////fim
